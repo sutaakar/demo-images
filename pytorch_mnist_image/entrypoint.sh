927d3f6de26dbd6d2d783481d164ea50baa27fc8
Karel Suta <ksuta@redhat.com>
2024-03-08 09:54:56 +0100

    fix

--- pytorch_mnist_image/entrypoint.sh
+++ pytorch_mnist_image/entrypoint.sh
@@ -1,28 +1,3 @@
 #!/bin/sh
 
-mkdir -p /usr/local/share/ca-certificates/certs
-chmod -R 755 /usr/local/share/ca-certificates/certs
-
-# Check if the SSL certificate files exist
-if [ -f "/etc/ssl/certs/odh-ca-bundle.crt" ]; then
-    echo "SSL certificate found, proceeding..."
-    cp /etc/ssl/certs/odh-ca-bundle.crt /usr/local/share/ca-certificates/certs/odh-ca-bundle.crt
-else
-    echo "SSL certificate not found"
-    mkdir -p /usr/local/share/ca-certificates/certs
-    touch /usr/local/share/ca-certificates/certs/odh-ca-bundle.crt
-    echo \
-'-----BEGIN CERTIFICATE-----
-<certificate-content-here>
------END CERTIFICATE-----' >> /usr/local/share/ca-certificates/certs/odh-ca-bundle.crt
-    update-ca-certificates
-    export REQUESTS_CA_BUNDLE=/usr/local/share/ca-certificates/certs/odh-ca-bundle.crt 
-    if [ -f "/usr/local/share/ca-certificates/certs/odh-ca-bundle.crt" ]; then
-        echo "exists"
-    fi
-    cat /usr/local/share/ca-certificates/certs/odh-ca-bundle.crt
-    echo $REQUESTS_CA_BUNDLE
-fi
-
-
 torchrun /test/mnist.py
